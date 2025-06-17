f06723fba2a4359186cb55ca2d97fb918f5c64ac
zjlim8 <zjlim8@gmail.com>
2025-06-17 23:59:13 +0800

    styling setup

---
+++ tailwind.config.js
@@ -0,0 +1,18 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./app/**/*.{js,ts,jsx,tsx}"],
+  presets: [require("nativewind/preset")],
+  theme: {
+    extend: {
+      colors: {
+        primary: "#3B82F6",
+        auxiliary: "#90A4AE",
+        headingtext: "#323232",
+        bodytext: "#5C5C5C",
+        buttontext: "#FDFDFD",
+        linktext: "#7C7C7C",
+      },
+    },
+  },
+  plugins: [],
+};
