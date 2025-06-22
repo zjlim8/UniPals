5afdd54ad037f6d8b422a644cab2604d1aa00f85
zjlim8 <zjlim8@gmail.com>
2025-06-22 21:26:31 +0800

    setting up dropdown for accountsetup

--- app/accountsetup.tsx
+++ app/accountsetup.tsx
@@ -1,11 +1,61 @@
+import CustomTextInput from "@/components/CustomTextInput";
+import DefaultButton from "@/components/DefaultButton";
 import React from "react";
-import { Text, View } from "react-native";
+import { Keyboard, Text, TouchableWithoutFeedback, View } from "react-native";
+
+const OPTIONS = [
+  { label: "Male", value: "male" },
+  { label: "Female", value: "female" },
+  { label: "Other", value: "other" },
+];
 
 const accountsetup = () => {
+  const [text, setText] = React.useState("");
+  const [gender, setGender] = React.useState<string>();
+
   return (
-    <View>
-      <Text>accountsetup</Text>
-    </View>
+    <TouchableWithoutFeedback
+      onPress={() => {
+        Keyboard.dismiss();
+      }}
+    >
+      <View className="screen justify-between">
+        <View>
+          <Text className="headtext mb-2">Tell us more about you!</Text>
+          <Text className="text-sm text-bodytext">
+            Please enter your details to complete your profile
+          </Text>
+        </View>
+        <View className="gap-3">
+          <CustomTextInput
+            label="First Name"
+            value={text}
+            onChangeText={(text) => setText(text)}
+          />
+          <CustomTextInput
+            label="Last Name"
+            value={text}
+            onChangeText={(text) => setText(text)}
+          />
+          <CustomTextInput
+            label="Course"
+            value={text}
+            onChangeText={(text) => setText(text)}
+          />
+          <CustomTextInput
+            label="Semester"
+            value={text}
+            onChangeText={(text) => setText(text)}
+          />
+        </View>
+        <DefaultButton
+          mode="contained"
+          onPress={() => console.log("Pressed")} // function to handle press button
+        >
+          Next
+        </DefaultButton>
+      </View>
+    </TouchableWithoutFeedback>
   );
 };
 
