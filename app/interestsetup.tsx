ca4f80cd0c043e8e19a1efc91d438e3e440a0bb9
zjlim8 <zjlim8@gmail.com>
2025-06-22 20:17:09 +0800

    front-end for interest setup

--- app/interestsetup.tsx
+++ app/interestsetup.tsx
@@ -1,14 +1,38 @@
 import DefaultButton from "@/components/DefaultButton";
+import ToggleButton from "@/components/ToggleButton";
 import { images } from "@/constants/images";
 import React from "react";
-import { Image, Text, View } from "react-native";
+import { Image, ScrollView, Text, View } from "react-native";
 
 const interestsetup = () => {
   return (
     <View className="screen justify-between ">
-      <View className="gap-5 mt-[80]">
+      <View className="flex-1 gap-5 mt-[80]">
         <Image source={images.interests} />
         <Text className="headtext text-left">What are your interests?</Text>
+        <ScrollView
+          className="flex-1 w-full h-full"
+          contentContainerStyle={{ paddingBottom: 24 }}
+          showsVerticalScrollIndicator={false}
+        >
+          <View className="flex-row flex-wrap">
+            <ToggleButton
+              label="Business"
+              selected={false}
+              onPress={() => console.log("Technology Pressed")}
+            />
+            <ToggleButton
+              label="Chess"
+              selected={true}
+              onPress={() => console.log("Technology Pressed")}
+            />
+            <ToggleButton
+              label="MMA"
+              selected={true}
+              onPress={() => console.log("Technology Pressed")}
+            />
+          </View>
+        </ScrollView>
       </View>
       <DefaultButton
         mode="contained"
---
+++ components/ToggleButton.tsx
@@ -0,0 +1,41 @@
+import React from "react";
+import { Button } from "react-native-paper";
+
+interface ToggleButtonProps {
+  label: string;
+  selected: boolean;
+  onPress: () => void;
+}
+
+export default function ToggleButton({
+  label,
+  selected,
+  onPress,
+}: ToggleButtonProps) {
+  return (
+    <Button
+      mode={selected ? "contained" : "outlined"}
+      buttonColor={selected ? "#3B82F6" : "transparent"}
+      textColor={selected ? "#FDFDFD" : "#3B82F6"}
+      onPress={onPress}
+      style={{
+        borderRadius: 10,
+        margin: 4,
+        borderColor: "#3B82F6",
+        borderWidth: 1,
+      }}
+      contentStyle={{
+        justifyContent: "center",
+        alignItems: "center",
+      }}
+      labelStyle={{
+        fontSize: 14,
+        lineHeight: 30,
+        textAlign: "center",
+        textAlignVertical: "center",
+      }}
+    >
+      {label}
+    </Button>
+  );
+}
